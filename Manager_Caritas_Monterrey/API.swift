2470a1e29c72f2174b7f183fc08d4ccdca6c3a93
Eduardo Francisco Lugo Quintana <65413364+VMink@users.noreply.github.com>
2023-11-12 20:11:00 -0600

    Update API.swift
    Final version of manager app

--- Manager_Caritas_Monterrey/API.swift
+++ Manager_Caritas_Monterrey/API.swift
@@ -7,60 +7,59 @@
 
 import Foundation
 
-struct Card: Codable, Identifiable {
-    var DIRECCION: String
-    var ESTATUS_PAGO: Int
-    var IMPORTE: Float
-    var REFERENCIA_DOMICILIO: String
-    var TEL_CASA: String
-    var TEL_MOVIL: String
-    var id: Int
-    
+struct Recolector {
+    let message: String
 }
 
-func callApi() -> Array<Card>{
-    var cards: Array<Card> = []
+func loginManager(username: String, password: String, completion: @escaping (Int) -> Void) {
     
-    print("Entrando a API")
+    let apiUrl = URL(string: "http://10.14.255.85:8085/loginManager")!
     
-    guard let url = URL(string: "http://10.14.255.85:8082/datosDiarios") else {
-        return cards
-    }
+    let parameters: [String: Any] = [
+        "USUARIO": username,
+        "PASS": password
+    ]
     
-    let group = DispatchGroup()
-    group.enter()
-    
-    let task = URLSession.shared.dataTask(with: url){ data, response, error in
-        let jsonDecoder = JSONDecoder()
-        if (data != nil) {
-            do {
-                let cardList = try jsonDecoder.decode([Card].self, from: data!)
-                
-                print("Lista \(cardList) ")
-                
-                for cardItem in cardList {
-                    print("Id: \(cardItem.id) - Direccion \(cardItem.DIRECCION)")
-                }
-                cards = cardList
-            } catch {
-                print(error)
+    do {
+        let jsonData = try JSONSerialization.data(withJSONObject: parameters)
+        
+        var request = URLRequest(url: apiUrl)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error: \(error)")
+                let userID = 2
+                completion(2)
+                return
             }
-            if let datosAPI = String(data: data!, encoding: .utf8) {
-                print(datosAPI)
-            }
-        }else{
             
-            print("No data")
+            if let data = data {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        let message = (json["message"] as? String) ?? ""
+                        if (message == "Authentication successful") {
+                            let userID = 1
+                            completion(1)
+                        } else {
+                            let userID = 0
+                            completion(0)
+                        }
+                    } else {
+                        print("Error: Unable to parse API response as JSON.")
+                        completion(0) // or handle the error in a way that makes sense for your application
+                    }
+                } catch {
+                    print("Error parsing JSON: \(error)")
+                    completion(0)
+                }
+            }
         }
-        group.leave()
+        task.resume()
+    } catch {
+        print("Error encoding JSON: \(error)")
+        completion(0)
     }
-    task.resume()
-    group.wait()
-    
-    print("******************************************")
-    print("Lista2: \(cards) ")
-    
-    return cards
 }
-
-var listaCards = callApi()
