5ca20622dca6c38c4060d8b107e75cf17565c1c7
Maffergu <60632469+Maffergu@users.noreply.github.com>
2023-11-23 14:10:04 -0600

    Hide keyboard

--- Manager_Caritas_Monterrey/View/ContentView.swift
+++ Manager_Caritas_Monterrey/View/ContentView.swift
@@ -107,12 +107,22 @@ struct ContentView: View {
             
         }
         navigationBarBackButtonHidden(true)
+            .onTapGesture {
+                        hideKeyboard()
+                }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func hideKeyboard() {
+        let resign = #selector(UIResponder.resignFirstResponder)
+        UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
     }
 }
 
